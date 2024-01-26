d2523e56b9f80a3c8dc373cafed185c1190edbd3
kej-simuca <157486823+kej-simuca@users.noreply.github.com>
2024-01-26 22:54:01 +0100

    Add files via upload

---
+++ ADimitri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    // Concrete product class representing the hero specific to Anastasia
+    public class ADimitri : IHero
+    {
+        public void Act()
+        {
+            Console.WriteLine("Dimitri. He Atacks Rasputin and his creatures.");
+        }
+    }
+}
